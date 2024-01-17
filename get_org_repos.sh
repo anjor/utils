366c68f30ed935932cf1e3c0a9195a8ca42f9209
anjor <anjor@umd.edu>
2024-01-17 06:05:36 -0500

    pagination

--- get_org_repos.sh
+++ get_org_repos.sh
@@ -1,38 +1,56 @@
 #!/usr/bin/env bash
 
 # Set your GitHub organization and personal access token
-ORG_NAME="filecoin-project"
-ACCESS_TOKEN=$(cat .env | cut -f 2 -d "=")
+ORG_NAME="ipni"
+ACCESS_TOKEN=$(grep "GITHUB_TOKEN" .env | cut -f 2 -d "=")
 
-# Make API request to get repositories
-response=$(curl -s -H "Authorization: token $ACCESS_TOKEN" "https://api.github.com/orgs/$ORG_NAME/repos")
+# Set pagination variables
+PAGE=1
+PER_PAGE=100
 
-# Check if the request was successful
-if [ $? -ne 0 ]; then
-  echo "Error making API request"
-  exit 1
-fi
+# Function for making paginated API request
+make_request() {
+  curl -s -H "Authorization: token $ACCESS_TOKEN" "https://api.github.com/orgs/$ORG_NAME/repos?per_page=$PER_PAGE&page=$PAGE"
+}
 
-# Parse repository names from the JSON response
-repo_names=$(echo "$response" | jq -r '.[].name')
+# Process results function
+function process_results() {
+  if [ $? -ne 0 ]; then
+    echo "Error making API request"
+    return 1
+  fi
+
+  repo_names=$(echo "$response" | jq -r '.[].name')
+
+  for repo in $repo_names; do
+    contributors_response=$(curl -s -H "Authorization: token $ACCESS_TOKEN" "https://api.github.com/repos/$ORG_NAME/$repo/contributors")
+
+    # Check if the request was successful
+    if [ $? -ne 0 ]; then
+      echo "Error making API request for contributors in $repo repository"
+      continue
+    fi
+
+    # Parse contributor names from the JSON response and get the top 
+    top_contributors=$(echo "$contributors_response" | jq -r '.[0].login' | tr '\n' ',' | sed 's/,$//')
+
+    # Print organization, repository, and top contributors
+    echo "$ORG_NAME, $repo,  $top_contributors"
+  done
+}
 
 # Print CSV header
 echo "Organization, Repository, Top_Contributors"
 
-# Loop through each repository and get top contributors
-for repo in $repo_names; do
-  contributors_response=$(curl -s -H "Authorization: token $ACCESS_TOKEN" "https://api.github.com/repos/$ORG_NAME/$repo/contributors")
+# Initial request
+response=$(make_request)
 
-  # Check if the request was successful
-  if [ $? -ne 0 ]; then
-    echo "Error making API request for contributors in $repo repository"
-    continue
-  fi
+# Process initial page of results
+process_results "$response"
 
-  # Parse contributor names from the JSON response and get the top 
-  top_contributors=$(echo "$contributors_response" | jq -r '.[0].login' | tr '\n' ',' | sed 's/,$//')
-
-  # Print organization, repository, and top contributors
-  echo "$ORG_NAME, $repo,  $top_contributors"
+# Paginate until no more results
+while [ $? -eq 0 ]; do
+  PAGE=$((PAGE+1))
+  response=$(make_request)
+  process_results "$response"
 done
-
